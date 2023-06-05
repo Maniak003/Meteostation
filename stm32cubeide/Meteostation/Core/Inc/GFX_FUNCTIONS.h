dbffc210f6efb2638058eb65063f7823b03db206
Maniak003 <ed@ultrastar.ru>
2023-06-05 08:11:17 +0300

    новый файл:    stm32cubeide/Meteostation/Core/Inc/GFX_FUNCTIONS.h 	новый файл:    stm32cubeide/Meteostation/Core/Inc/ST7735_fonts.h 	новый файл:    stm32cubeide/Meteostation/Core/Inc/st7735.h 	новый файл:    stm32cubeide/Meteostation/Core/Src/GFX_FUNCTIONS.c 	новый файл:    stm32cubeide/Meteostation/Core/Src/ST7735_fonts.c 	новый файл:    stm32cubeide/Meteostation/Core/Src/st7735.c

---
+++ stm32cubeide/Meteostation/Core/Inc/GFX_FUNCTIONS.h
@@ -0,0 +1,21 @@
+#ifndef INC_GFX_FUNCTIONS_H_
+#define INC_GFX_FUNCTIONS_H_
+
+
+void drawPixel(int16_t x, int16_t y, uint16_t color);
+void writeLine(int16_t x0, int16_t y0, int16_t x1, int16_t y1, uint16_t color);
+void  drawFastVLine(int16_t x, int16_t y, int16_t h, uint16_t color);
+void  drawFastHLine(int16_t x, int16_t y, int16_t w, uint16_t color);
+void fillRect(int16_t x, int16_t y, int16_t w, int16_t h, uint16_t color);
+void drawLine(int16_t x0, int16_t y0, int16_t x1, int16_t y1, uint16_t color);
+void drawCircle(int16_t x0, int16_t y0, int16_t r, uint16_t color);
+void drawCircleHelper( int16_t x0, int16_t y0, int16_t r, uint8_t cornername, uint16_t color);
+void fillCircleHelper(int16_t x0, int16_t y0, int16_t r, uint8_t corners, int16_t delta, uint16_t color);
+void fillCircle(int16_t x0, int16_t y0, int16_t r, uint16_t color);
+void drawRect(int16_t x, int16_t y, int16_t w, int16_t h, uint16_t color);
+void drawRoundRect(int16_t x, int16_t y, int16_t w, int16_t h, int16_t r, uint16_t color);
+void fillRoundRect(int16_t x, int16_t y, int16_t w, int16_t h, int16_t r, uint16_t color);
+void drawTriangle(int16_t x0, int16_t y0, int16_t x1, int16_t y1, int16_t x2, int16_t y2, uint16_t color);
+void fillTriangle(int16_t x0, int16_t y0, int16_t x1, int16_t y1, int16_t x2, int16_t y2, uint16_t color);
+
+#endif
